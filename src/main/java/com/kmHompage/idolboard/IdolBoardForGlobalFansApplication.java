acba7e3858be2664c68a4265d454b1edba938b40
lenagend <57205046+lenagend@users.noreply.github.com>
2023-03-02 10:02:39 +0900

    2023-03-02 [Initial commit]

---
+++ src/main/java/com/kmHompage/idolboard/IdolBoardForGlobalFansApplication.java
@@ -0,0 +1,13 @@
+package com.kmHompage.idolboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdolBoardForGlobalFansApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdolBoardForGlobalFansApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kmHompage/idolboard/IdolBoardForGlobalFansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kmHompage.idolboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdolBoardForGlobalFansApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
