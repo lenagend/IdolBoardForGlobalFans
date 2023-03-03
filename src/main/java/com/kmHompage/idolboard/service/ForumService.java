5e9ca754f4a08101b8ab1d4f8ffaa4ef170c6500
lenagend <57205046+lenagend@users.noreply.github.com>
2023-03-03 12:55:40 +0900

    2023-03-03 [add save ForumService and test]

--- src/main/java/com/kmHompage/idolboard/service/ForumService.java
+++ src/main/java/com/kmHompage/idolboard/service/ForumService.java
@@ -4,6 +4,7 @@ import com.kmHompage.idolboard.domain.Board;
 import com.kmHompage.idolboard.repository.BoardRepository;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 public class ForumService {
@@ -15,5 +16,6 @@ public class ForumService {
     }
 
     public Flux<Board> getForums(){return this.boardRepository.findAll();}
+    Mono<Board> saveForum(Board board){return this.boardRepository.save(board);}
 
 }
--- src/test/java/com/kmHompage/idolboard/service/ForumServiceUnitTest.java
+++ src/test/java/com/kmHompage/idolboard/service/ForumServiceUnitTest.java
@@ -3,9 +3,12 @@ package com.kmHompage.idolboard.service;
 import com.kmHompage.idolboard.domain.Board;
 import com.kmHompage.idolboard.repository.BoardRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -23,6 +26,21 @@ public class ForumServiceUnitTest {
     void setup(){
         Board sampleBoard = new Board( "Blackpink", "Yg Entertainment's girl group");
         Board sampleBoard2 = new Board("twice", "JYP Entertainment's girl group");
+
+        when(boardRepository.save(any(Board.class))).thenReturn(Mono.just(sampleBoard));
+
+        forumService = new ForumService(boardRepository);
+    }
+
+    @Test
+    void addForum(){
+        forumService.saveForum(new Board( "Blackpink", "Yg Entertainment's girl group"))
+                .as(StepVerifier::create)
+                .expectNextMatches(board -> {
+                    assertThat(board.getName()).isEqualTo("Blackpink");
+                    return true;
+                }).verifyComplete();
+
     }
 
 
